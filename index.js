c8073636d5c482c2d45986df8d4208fc9f7e5d47
Fasika Belayneh <fafibelayneh@gmail.com>
2024-09-23 19:57:03 +0300

    Updated the current time and date

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function weather(response) {
   let info = Math.round(response.data.daily[0].temperature.day);
   let detail = document.querySelector(".amount");
   detail.innerHTML = `${info}`;
+  let selectDescribe = document.querySelector(".describe");
+  selectDescribe.innerHTML = `${response.data.daily[0].condition.description}`;
 }
 function search(city) {
   let apiKey = "90c47000f520956f67b7e0t1do4a3be3";
@@ -22,3 +24,24 @@ function show(event) {
 }
 form.addEventListener("submit", show);
 let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let selectDay = document.querySelector(".day");
+selectDay.innerHTML = `${days[now.getDay()]}`;
+let selectHour = document.querySelector(".hour");
+selectHour.innerHTML = `${now.getHours()}`;
+let selectMinute = document.querySelector(".minute");
+selectMinute.innerHTML = `${now.getMinutes()}`;
+if (now.getMinutes() < 10) {
+  selectMinute.innerHTML = `0${now.getMinutes()}`;
+} else {
+  selectMinute.innerHTML = `${now.getMinutes()}`;
+}
