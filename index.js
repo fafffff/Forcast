ec9d3772724389a9ea2081cd3b98f879a4091da2
Fasika Belayneh <fafibelayneh@gmail.com>
2024-09-23 22:22:50 +0300

    integrating the weather emoji with the API

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function weather(response) {
   }`;
   let selectWind = document.querySelector(".wind");
   selectWind.innerHTML = `${response.data.daily[now.getDay()].wind.speed}`;
+  let selectIcon = document.querySelector(".icon");
+  selectIcon.innerHTML = `<img src="${
+    response.data.daily[now.getDay()].condition.icon_url
+  }"alt="weather-image" class="icon"/>`;
+  console.log(`${response.data.daily[now.getDay()].condition.icon_url}`);
 }
 function search(city) {
   let apiKey = "90c47000f520956f67b7e0t1do4a3be3";
@@ -23,12 +28,12 @@ function search(city) {
   console.log(apiURL);
 }
 let form = document.querySelector(".search-form");
-function show(event) {
+function show(event, response) {
   event.preventDefault();
   let input = document.querySelector("#search-form-input");
   let inputCity = document.querySelector("#cities");
   inputCity.innerHTML = `${input.value}`;
-  search(input.value);
+  search(response.data.city);
 }
 form.addEventListener("submit", show);
 let now = new Date();
@@ -53,4 +58,3 @@ if (now.getMinutes() < 10) {
 } else {
   selectMinute.innerHTML = `${now.getMinutes()}`;
 }
-console.log(response.data.daily[now.getDay()].temperature.day);
