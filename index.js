bb34609261d681c93575f79ae508c0dba4362b77
Fasika Belayneh <fafibelayneh@gmail.com>
2024-09-24 14:35:07 +0300

    inject the forecast HTML from javascript

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function weather(response) {
-  //detail.innerHTML = response.data.temperature;
-
+  let input = document.querySelector("#search-form-input");
+  if (!response.data.city.toLowerCase().includes(input.value.toLowerCase())) {
+    if (
+      confirm(
+        `Sorry, ${input.value} is not included. Would you like to search on Google for ${input.value}?`
+      )
+    ) {
+      window.open(
+        `https://www.google.com/search?q=weather+${input.value}`,
+        "_blank"
+      );
+    }
+  }
   let info = Math.round(response.data.daily[now.getDay()].temperature.day);
   let detail = document.querySelector(".amount");
   detail.innerHTML = `${info}`;
@@ -20,6 +31,7 @@ function weather(response) {
   }"alt="weather-image" class="icon"/>`;
   console.log(`${response.data.daily[now.getDay()].condition.icon_url}`);
 }
+
 function search(city) {
   let apiKey = "90c47000f520956f67b7e0t1do4a3be3";
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -33,7 +45,7 @@ function show(event) {
   let input = document.querySelector("#search-form-input");
   let inputCity = document.querySelector("#cities");
   inputCity.innerHTML = `${input.value}`;
-  search(input.value);
+  search(input.value.toLowerCase());
 }
 form.addEventListener("submit", show);
 let now = new Date();
@@ -58,3 +70,23 @@ if (now.getMinutes() < 10) {
 } else {
   selectMinute.innerHTML = `${now.getMinutes()}`;
 }
+function displayWeather() {
+  let day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let dailyForecast = "";
+  day.forEach(function (index) {
+    dailyForecast =
+      dailyForecast +
+      `<div class="weather-forcast-mon">
+            <div class="Mon">${index}</div>
+            <div class="Mon-icon">⛅️</div>
+            <div class="Mon-temp">
+              <div class="Mon-higher-temp"><b>15°</b></div>
+              <div class="Mon-lower-temp">16°</div>
+            </div>
+          </div>`;
+  });
+  let weatherForecast = document.querySelector(".weather-forecast");
+  weatherForecast.innerHTML = dailyForecast;
+}
+displayWeather();
